814487ec97d1847a3414b4b5a26f1886458b7e13
Soliman Soliman <93845329+solimanprogprojects@users.noreply.github.com>
2021-11-08 19:25:28 +1100

    Add files via upload

--- wwwroot/js/loginErrors.js
+++ wwwroot/js/loginErrors.js
@@ -1,1 +1,17 @@
-
+﻿function checkInput()
+{
+    var userNameFault = "Username must be 8 characters long. Please reenter.";
+    var passwordFault = "Password must be 8 characters long. Please reenter";
+    var userName = document.getElementById("userNameTxt").value;
+    var password = document.getElementById("pwdTxt").value;
+    if (userName.length < 8)
+    {
+        alert(userNameFault);
+        return false;
+    }
+    if (password.length < 8)
+    {
+        alert(passwordFault);
+        return false
+    }
+}
